e8572d8443c2c3ba135c34d36b67b1ed2db8c9fa
solomon ogu <solomonogu0@gmail.com>
2025-02-13 14:42:53 +0100

    chore: remove proxy-agent header

--- src/reqs.c
+++ src/reqs.c
@@ -308,7 +308,6 @@ static int send_connect_method_response (struct conn_s *connptr)
 {
         return write_message (connptr->client_fd,
                               "HTTP/1.%u 200 Connection established\r\n"
-                              "Proxy-agent: " PACKAGE "\r\n"
                               "\r\n", connptr->protocol.major != 1 ? 0 :
                                       connptr->protocol.minor);
 }
